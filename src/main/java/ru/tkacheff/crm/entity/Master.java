44eb64ca879089309e8e393fdccab3e19dc0eb3e
timtkacheff <timtkacheff@gmail.com>
2022-04-19 19:37:45 +0300

    minor master changes

--- src/main/java/ru/tkacheff/crm/entity/Master.java
+++ src/main/java/ru/tkacheff/crm/entity/Master.java
@@ -1,12 +1,9 @@
 package ru.tkacheff.crm.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 
@@ -19,22 +16,28 @@ public class Master {
     private Integer id;
 
     @Column(name = "name")
-    @Getter @Setter
+    @Getter
+    @Setter
     private String name;
 
     @Column(name = "phone_number")
-    @Getter @Setter
+    @Getter
+    @Setter
     private String phoneNumber;
 
     @Column(name = "specialization")
-    @Getter @Setter
+    @Getter
+    @Setter
     private String specialization;
 
     @Column(name = "hourly_rate")
-    @Getter @Setter
+    @Getter
+    @Setter
     private Double hourlyRate;
 
-    public Master() {}
+    public Master() {
+
+    }
 
     @Builder
     public Master(String name, String phoneNumber, String specialization, Double hourlyRate) {
